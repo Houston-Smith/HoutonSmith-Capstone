af64e52bba8d83aaae8456ddf9130252813a0e82
Houston-Smith <houstonbsmith@gmail.com>
2022-04-26 14:42:49 -0500

    Displays Crew Members on Crew Card

--- src/components/crews/CrewCard.js
+++ src/components/crews/CrewCard.js
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getHideoutById } from "../../modules/HideoutManager";
+import { getCroniesByCrew } from "../../modules/CronyManager";
 
 export const CrewCard = ({crew, callDeleteCrew}) => {
 
   const navigate = useNavigate()
 
   const [hideout, setHideout] = useState({ name: "", location: "" });
+  const [cronies, setCronies] = useState([])
+
+  useEffect(() => {
+    getCroniesByCrew(crew.id)
+      .then(cronyList => {
+        console.log(cronyList)
+        setCronies(cronyList);
+      });
+  }, []);
 
   useEffect(() => {
     getHideoutById(crew.hideoutId)
@@ -23,6 +33,12 @@ export const CrewCard = ({crew, callDeleteCrew}) => {
         <h2>{crew.name}</h2>
         <p>Hideout: {hideout.name}</p>
         <p>{crew.description}</p>
+        <p>{crew.description}</p>
+        {cronies.map(c => (
+						<p key={c.id}>
+								{c.name}
+						</p>
+						))}  
         <button type="button" className="btn btn-primary" onClick={() => callDeleteCrew(crew.id)}>Disband</button>
         <button type="button" className="btn btn-primary" onClick={() => {navigate(`/crews/${crew.id}/edit`)}}>Edit</button>
       </div>
