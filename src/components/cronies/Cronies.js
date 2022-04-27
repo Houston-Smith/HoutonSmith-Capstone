25b6cefa04b8d765af1d5d0ada5c3d423648734e
Houston-Smith <houstonbsmith@gmail.com>
2022-04-27 14:21:05 -0500

    Crony Filter by Crew

--- src/components/cronies/Cronies.js
+++ src/components/cronies/Cronies.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "./Cronies.css";
 import { CronyCard } from "./CronyCard";
 import { deleteCrony, getCroniesOfActiveUser } from "../../modules/CronyManager";
+import { getCroniesByCrew } from "../../modules/CronyManager";
+import { getAllCrews } from "../../modules/CrewManager";
 
 //----------------------------------------------BROUGHT TO YOU BY HOUSTON SMITH---------------------------------------------------------//
 
@@ -20,10 +22,10 @@ export const Cronies = () => {
   const navigate = useNavigate()
 
 
-//---------------------------------------------------SET EMPTY CRONIES ARRAY-------------------------------------------------------------//
+//---------------------------------------------------SET EMPTY CRONIESAND CREWS ARRAYS----------------------------------------------------//
 
   const [cronies, setCronies] = useState([])
-
+  const [crews, setCrews] = useState([])
   
 
 //-----------------------------------POPULATE EMPTY CRONIES ARRAY WITH OBJECTS FROM THE API----------------------------------------------//
@@ -36,11 +38,23 @@ export const Cronies = () => {
     })
   }
 
+  const getCrews = () => {
+    //Pull Crews array for the active user from API...
+    return getAllCrews(currentUser).then(crews => {
+      //...then populate empty crews array with what comes back.
+      setCrews(crews)
+    })
+  }
 
-//------------------------------------------RUN getCronies FUNCTION AFTER FIRST RENDER---------------------------------------------------//
+
+//----------------------------------------RUN getCronies AND getCrews FUNCTIONS AFTER FIRST RENDER------------------------------------------//
 
 useEffect(() => {
   getCronies()
+}, [])
+
+useEffect(() => {
+  getCrews()
 }, [])
 
 //--------------------------------------------CALLS THE deleteCrony FUNCTION-------------------------------------------------------------//
@@ -50,12 +64,46 @@ const callDeleteCrony = (id) => {
   .then(() => getCronies())
 };
 
+const filterCronies = (event) => {
+
+  // save the input as a variable
+  let selectedVal = event.target.value
+  // forms always provide values as strings. But we want to save the ids as numbers.
+  if (event.target.id.includes("Id")) {
+    selectedVal = parseInt(selectedVal)
+  }
+
+  //if selected value is not an empy string: Filter by the crew Id
+  if (selectedVal != "") {
+  getCroniesByCrew(selectedVal)
+    .then(cronies => setCronies(cronies))
+
+  //otherwise display all cronies
+  } else {
+    getCronies()
+  } 
+}
+
 
  //--------------------------------GENERATE HTML FOR HIDEOUTS PAGE AND GENERATE HIDEOUT CARDS------------------------------------------------// 
 
   return (
     <main>
       <section className="friend-header">
+      <fieldset>
+				<div className="form-group">
+					<label htmlFor="crew">Filter Cronies:</label>
+					<select name="crewFilter" id="crewFilter" onChange={filterCronies} className="form-control" >
+            <option value="">All Cronies</option>
+            <option value="0">Crewless Cronies</option>
+						{crews.map(c => (
+						<option key={c.id} value={c.id}>
+								{c.name}
+						</option>
+						))}
+					</select>
+				</div>
+			</fieldset>
         <h1>Crony List</h1>
         <button type="button" className="btn btn-primary" onClick={() => {navigate("/cronies/add")}}>Crony Applications</button>
       </section>
