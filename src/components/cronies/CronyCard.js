9cc6b499e9b75c515003aa0720b6ad68f19c1795
Houston-Smith <houstonbsmith@gmail.com>
2022-04-27 16:20:11 -0500

    Log out Commit

--- src/components/cronies/CronyCard.js
+++ src/components/cronies/CronyCard.js
@@ -19,7 +19,6 @@ export const CronyCard = ({crony, callDeleteCrony}) => {
     //Pull Crews array for the active user from API...
     return getCrewById(crony.crewId).then(crew => {
       //...then populate empty crews array with what comes back.
-      console.log(crew)
       setCrew(crew)
     })
   }
